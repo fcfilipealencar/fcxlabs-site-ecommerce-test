e477b427f3d9373d73108c189284653e6eeddb78
filipe.alencar <filipe.alencar@ferreiracosta.com.br>
2022-10-04 16:09:59 -0300

    callbacks

--- pages/sign-in/apple.tsx
+++ pages/sign-in/apple.tsx
@@ -7,7 +7,10 @@ const SignInPage = () => {
     const { data: session, status } = useSession();
     useEffect(() => {
         if (!(status === "loading") && !session)
-            void signIn("apple", { callbackUrl: "/sign-in/apple" });
+            void signIn("apple", {
+                // eslint-disable-next-line no-restricted-globals
+                callbackUrl: `${location.origin}/sign-in/apple`,
+            });
 
         if (session) window.close();
     }, [session, status]);
--- pages/sign-in/google.tsx
+++ pages/sign-in/google.tsx
@@ -7,7 +7,10 @@ const SignInPage = () => {
     const { data: session, status } = useSession();
     useEffect(() => {
         if (!(status === "loading") && !session)
-            void signIn("google", { callbackUrl: "/sign-in/google" });
+            void signIn("google", {
+                // eslint-disable-next-line no-restricted-globals
+                callbackUrl: `${location.origin}/sign-in/google`,
+            });
 
         if (session) window.close();
     }, [session, status]);
