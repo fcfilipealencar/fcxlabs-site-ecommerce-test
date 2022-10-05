7b4f51e757390bf32d6335311fc275ff78d4337a
filipe.alencar <filipe.alencar@ferreiracosta.com.br>
2022-10-05 16:56:39 -0300

    route

--- pages/sign-in/apple.tsx
+++ pages/sign-in/apple.tsx
@@ -1,19 +1,20 @@
 /* eslint-disable no-void */
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { ContentSignInStyle, SignInStyle } from "./styles";
 
 const SignInPage = () => {
     const { data: session, status } = useSession();
+    const router = useRouter();
     useEffect(() => {
         if (!(status === "loading") && !session)
-            void signIn("apple", {
-                // eslint-disable-next-line no-restricted-globals
-                callbackUrl: `${location.origin}/sign-in/google`,
-                redirect: true,
+            void signIn("apple").then(() => {
+                return router.push("/sign-in/apple");
             });
 
         if (session) window.close();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [session, status]);
 
     return (
